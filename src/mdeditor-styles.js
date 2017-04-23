e4492c7aadff42724c0e6d95d272f6a4c0bd292e
NtimYeboah <lylegendary@gmail.com>
2017-04-23 22:39:11 +0000

    Add styles for mdeditor

---
+++ src/mdeditor-styles.js
@@ -0,0 +1,56 @@
+/**
+ * Created by ntimobedyeboah on 4/23/17.
+ */
+let textAreaStyle =  {
+    borderColor: '#dbdbdb',
+    color: '#363636',
+    boxShadow: 'inset 0 1px 2px rgba(10, 10, 10, 0.1)',
+    fontFamily: 'BlinkMacSystemFont, -apple-system, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", ' +
+    '"Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", "Helvetica", "Arial", sans-serif',
+    userSelect: 'text',
+    cursor: 'auto',
+    whiteSpace: 'pre-wrap',
+    wordWrap: 'break-word',
+    borderRadius: 3,
+    padding: 0.625 + 'em',
+    resize: 'vertical',
+    letterSpacing: 'normal',
+    wordSpacing: 'normal',
+    textTransform: 'none',
+    textIndent: 0,
+    textShadow: 'none',
+    textAlign: 'start',
+    fontSize: 16
+},
+
+buttonStyle = {
+    alignItems: 'center',
+    border: '1px solid transparent',
+    borderRadius: 3,
+    boxShadow: 'none',
+    display: 'inline-flex',
+    fontSize: 1 + 'rem',
+    height: 2.25 + 'em',
+    lineHeight: 1.5,
+    paddingBottom: 'calc(0.375em - 1px)',
+    paddingTop: 'calc(0.375em - 1px)',
+    verticalAlign: 'top',
+    userSelect: 'none',
+    backgroundColor: 'white',
+    borderColor: '#dbdbdb',
+    color: '#363636',
+    cursor: 'pointer',
+    justifyContent: 'center',
+    paddingLeft: 0.75 + 'em',
+    paddingRight: 0.75 + 'em',
+    textAlign: 'center',
+    whiteSpace: 'nowrap',
+    transition: 'none 86ms ease-out',
+    fontWeight: 400
+};
+
+let styles = { textAreaStyle, buttonStyle };
+
+export default styles;
+
+
