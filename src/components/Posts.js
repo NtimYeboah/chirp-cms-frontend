309dca17056b050d3a2c49911f95ae75c729a983
NtimYeboah <lylegendary@gmail.com>
2017-04-27 10:16:29 +0000

    Fetch posts in Posts component

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -2,15 +2,50 @@
  * Created by ntimobedyeboah on 4/14/17.
  */
 import React, { Component } from 'react';
+import PostItem from './PostItem';
+import PostsUnavailable from './PostsUnavailable';
+import CreatePostButton from './CreatePostButton';
+import Post from './../services/post';
 
 class Posts extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {}
+    }
+
+    componentDidMount() {
+        Post.getAll()
+            .then(posts => {
+                this.setState({ posts });
+            })
+            .catch(err => {
+                throw new Error(err);
+            });
+    }
+
     render() {
+        let posts;
+
+        if (! this.state.posts) return (<PostsUnavailable />);
+
+        posts = this.state.posts.map(post => {
+            return (
+                <PostItem details={ post } key={ post.id } />
+            )
+        });
+
         return (
-            <div>
-                <h1>Post component</h1>
-            </div>
+            <section className="section">
+                <div className="container">
+                    <CreatePostButton />
+                    <div className="columns is-multiline">
+                        { posts }
+                    </div>
+                </div>
+            </section>
         )
     }
 }
 
-module.exports = Posts;
+export default Posts;
